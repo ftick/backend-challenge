5064c8b44c2d7fe349624cc3fa79196ab99bf879
Ian Bantoto <lcbantoto@gmail.com>
2018-11-14 22:04:29 -0500

    Add Socket.IO package

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var express = require("express");
+var app = require("express")();
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
-var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3000, function () {
-    console.log("Server running at http://localhost:", server.address().port);
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+http.listen(3000, function () {
+    console.log("Server running at http://localhost:3000");
 });
