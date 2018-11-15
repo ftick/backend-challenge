1542738b3f8b1ea275dd82b8cd13b043a52dc6d2
Ian Bantoto <lcbantoto@gmail.com>
2018-11-14 22:49:18 -0500

    /data/:key endpoint checks if input is valid JSON

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 var app = require("express")();
-var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 routes(app);
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-});
+// io.on('connection', function(socket){
+//     console.log("a user connected");
+// });
 
 http.listen(3000, function () {
     console.log("Server running at http://localhost:3000");
--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,8 @@ var router = function (app) {
 
     var DIR = __dirname.split('\\').slice(0, -1).join('\\');
 
-    function read(cb) {
-        fs.readFile(DIR + '/data.txt', 'utf8', function(err, data) {
+    function readData(cb) {
+        fs.readFile(DIR + '/data.txt', 'utf8', (err, data) => {
             if (!err) {
                 cb(data.toString());
             } else {
@@ -14,33 +14,36 @@ var router = function (app) {
         });
     }
 
-    app.get("/", function (req, res) {
-        res.status(200).send({ message: 'Welcome to my backend REST API' });
+    app.get("/", (req, res) => {
+        res.sendFile(DIR + '/index.html');
     });
 
-    app.get("/data", function (req, res) {
-        read(function(data) {
+    app.get("/data", (req, res) => {
+        readData((data) => {
             res.status(200).send(JSON.parse(data));
         });
     });
 
-    app.post("/data/update", function(req, res) {
+    app.post("/data/update", (req, res) => {
+        if(!req.body) return res.status(400).send({ message: 'Data is not valid JSON' });
         var text = JSON.stringify(req.body, null, '\t');
-        if (text)
-        fs.writeFile(DIR + '/data.txt', text, function(err, data){
-            if (err) console.log(err);
-        });
+        if (text) {
+            fs.writeFile(DIR + '/data.txt', text, (err) => {
+                if (err) throw err;
+                else return res.status(200).send({ message: 'Update successful'});
+            });
+        }
     });
 
-    app.get("/data/:key", function(req, res) {
+    app.get("/data/:key", (req, res) => {
         var key = req.params.key;
-        read(function(data) {
+        readData((data) => {
             json = JSON.parse(data);
             value = json[key];
             if (value != undefined) {
                 res.status(200).send(value);
             } else {
-                res.status(400).send({ message: 'key not found' });
+                res.status(400).send({ message: key + ' is not a valid key' });
             }
         });
     });
